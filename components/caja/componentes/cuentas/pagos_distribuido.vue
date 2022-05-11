fbde1b0b428a07a6c2c769874fa3282679c4ec73
Williams Santos <willysantos_lai@gmail.com>
2022-05-10 19:57:43 -0600

    update 2.0.26 -modulo validacion de deposito

---
+++ components/caja/componentes/cuentas/pagos_distribuido.vue
@@ -0,0 +1,42 @@
+<template>
+<v-card flat>
+  <v-simple-table dense height="250" class="rowsTable" >
+    <template v-slot:default>
+      <thead>
+      <tr>
+        <th>Tipo</th>
+        <th>Descripción</th>
+        <th>Pendiente</th>
+        <th>Pagará</th>
+        <th>Saldo</th>
+        <th>Aún Hay</th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr v-for="item in pagos">
+        <td>{{item.tipo}}</td>
+        <td>
+          {{item.detalle}}
+        </td>
+        <td><strong>L. </strong>{{item.pendiente}}</td>
+        <td><strong>L. </strong>{{item.pagara}}</td>
+        <td><strong>L. </strong>{{item.saldo}}</td>
+        <td><strong>L. </strong>{{item.hay}}</td>
+      </tr>
+      </tbody>
+    </template>
+  </v-simple-table>
+</v-card>
+</template>
+
+<script>
+
+export default {
+  name: "pagos_distribuido",
+  props: {pagos: Array}
+}
+</script>
+
+<style scoped>
+
+</style>
--- components/contabilidad/deposito.vue
+++ components/contabilidad/deposito.vue
@@ -1,6 +1,6 @@
 <template>
   <v-card class="ma-2" flat>
-    <v-card>
+    <v-card :disabled="enviado">
       <v-row no-gutters>
         <v-col>
           <v-simple-table>
@@ -40,7 +40,7 @@
               </tr>
               <tr>
                 <th>Documento</th>
-                <td><b-link>Ver documento</b-link></td>
+                <td><b-link @click="verDocumento(DEPOSITO.file)">Ver documento</b-link></td>
               </tr>
               <tr>
                 <th>Fecha Revisado</th>
@@ -63,12 +63,12 @@
               </tr>
               <tr>
                 <th>Observaciones</th>
-                <td><v-btn small tile dark text color="indigo">Ver</v-btn></td>
+                <td><v-btn small tile dark text color="indigo" :disabled="DEPOSITO.estado === 1" @click="dialogoObseracion = true">Ver</v-btn></td>
               </tr>
               <tr>
                 <th>Pagos Editados</th>
                 <td>
-                  <v-btn small tile dark text color="indigo">Ver</v-btn>
+                  <v-btn small tile class="text-white" text color="indigo" :disabled="DEPOSITO.estado === 1" @click="dialogo = true">Ver</v-btn>
                 </td>
               </tr>
               </tbody>
@@ -78,16 +78,26 @@
       </v-row>
     </v-card>
 
-    <v-card-subtitle>Pagos pendientes</v-card-subtitle>
-    <v-data-table dense :headers="header" :loading="load" loading-text="Cargando pagos ..."
-                  :items="pagos" show-select v-model="pagosSelect" :items-per-page="5">
-      <template v-slot:item.saldo_actual="{item}">L. {{int.format(item.saldo_actual)}}</template>
-      <template v-slot:item.mora="{item}">L. {{int.format(item.mora)}}</template>
-    </v-data-table>
+
+    <v-row no-gutters>
+      <v-col>
+        <v-card-subtitle>Pagos pendientes</v-card-subtitle>
+        <v-data-table dense :headers="header" :loading="load" loading-text="Cargando pagos ..."
+                      :items="pagos" show-select v-model="pagosSelect" :items-per-page="5">
+          <template v-slot:item.saldo_actual="{item}">L. {{int.format(item.saldo_actual)}}</template>
+          <template v-slot:item.mora="{item}">L. {{int.format(item.mora)}}</template>
+        </v-data-table>
+      </v-col>
+      <v-divider vertical></v-divider>
+      <v-col>
+        <v-card-subtitle>Pagos a realizar</v-card-subtitle>
+        <pagos_distribuido :pagos="PagosDistrubuidos"/>
+      </v-col>
+    </v-row>
 
     <v-divider></v-divider>
 
-    <v-row no-gutters>
+    <v-row no-gutters v-if="DEPOSITO.estado === 1">
       <v-col cols="9">
         <v-card flat class="ma-2">
           <v-card-subtitle>Datos de la cuenta</v-card-subtitle>
@@ -100,33 +110,73 @@
         <v-card flat class="ma-2">
           <v-card-subtitle>Formulario</v-card-subtitle>
           <v-divider></v-divider>
-          <v-form>
-            <v-btn small block color="success" text tile dark @click="calcularSaldo">Calcular Saldos</v-btn>
-            <v-text-field label="Saldo en Depósito" v-model="DEPOSITO.monto_actual" disabled class="ma-2"></v-text-field>
-            <v-text-field label="Saldo a Abonar" disabled class="ma-2" v-model="saldo_abonado"></v-text-field>
+          <v-form ref="FormValidarDepostio">
+            <v-btn small block color="success" text tile dark @click="distribuirPago">Calcular Saldos</v-btn>
             <v-autocomplete label="Seleccionar un banco" v-model="cuenta"
-                            :items="Cuentas"></v-autocomplete>
-            <v-textarea label="Observaciones" class="ma-2" v-model="DEPOSITO.observacion"></v-textarea>
+                            :items="Cuentas" :rules="[deposito.req]"></v-autocomplete>
+            <v-textarea label="Observaciones" :rules="[deposito.req, deposito.min, deposito.max]"
+                        class="ma-2" v-model="DEPOSITO.observacion"></v-textarea>
           </v-form>
           <v-divider></v-divider>
-          <v-btn block color="indigo" @click="registrarForm" dark tile small>Registrar Validación</v-btn>
+          <v-btn block color="indigo" @click="validarForm" dark tile small>Registrar Validación</v-btn>
         </v-card>
       </v-col>
     </v-row>
+
+
+    <v-dialog v-model="dialogo" width="70%">
+      <v-card>
+        <v-toolbar color="grey lighten-3" flat>
+          <v-card-title>Pagos que se editaron</v-card-title>
+        </v-toolbar>
+        <v-container>
+          <pagos_distribuido v-if="DEPOSITO.estado === 0" :pagos="JSON.parse(DEPOSITO.pagos_editados)"/>
+
+          <v-divider></v-divider>
+          <v-card-actions class="d-flex justify-end">
+            <v-btn color="indigo" dark small tile @click="dialogo = false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-card>
+    </v-dialog>
+
+    <v-dialog v-model="dialogoObseracion" width="30%">
+      <v-card>
+        <v-toolbar color="grey lighten-3">
+          <v-card-text>Observación</v-card-text>
+        </v-toolbar>
+        <v-container>
+          <v-card-text>{{DEPOSITO.observacion}}</v-card-text>
+          <v-dialog></v-dialog>
+          <v-card-actions class="d-flex justify-end">
+            <v-btn color="indigo" dark small tile @click="dialogoObseracion = false">Cerrar</v-btn>
+          </v-card-actions>
+        </v-container>
+      </v-card>
+    </v-dialog>
   </v-card>
 </template>
 
 <script>
 import cuenta from "../../components/Ventas/cuenta";
+import {ipcRenderer} from "electron";
+import pagos_distribuido from "@/components/caja/componentes/cuentas/pagos_distribuido";
 export default {
   name: "deposito",
-  components:{cuenta},
+  components:{cuenta, pagos_distribuido},
   data(){
     return{
+      dialogo: false,
+      dialogoObseracion: false,
+      deposito:{
+        req: v => !!v || 'Campo requerido',
+        min:v => (v && v.length >= 5) || 'Tiene que ser mayor a 5 carácteres.',
+        max:v => (v && v.length <= 50) || 'Tiene que ser menor o igual a 50 carácteres.',
+      },
       int: Intl.NumberFormat(),
       pagos: [],
       pagosSelect: [],
-      pagosSi: [],
+      PagosDistrubuidos: [],
       saldo_abonado: 0,
       load: true,
       header:[
@@ -139,7 +189,8 @@ export default {
       Cuentas: [],
       loadCuentas: true,
       cuenta: '',
-      moraAbonado: 0
+      moraAbonado: 0,
+      enviado: false
     }
   },
   computed:{
@@ -149,6 +200,9 @@ export default {
     CUENTA(){
       return this.$store.state.cuentas.CUENTA;
     },
+    USUARIO(){
+      return this.$store.state.usuario;
+    }
   },
   created() {
     this.$axios.get('venta/pagos/'+this.DEPOSITO.venta.id).then((res)=>{
@@ -162,73 +216,15 @@ export default {
       this.load = false;
     });
     this.cargarCuentas();
+    this.$store.commit('activarOverlay', false);
   },
   methods:{
-    calcularSaldo(){
-      let mora = 0;
-      let sa   = 0;
-      let saa  = 0;
-      let sar  = 0;
-      if (this.DEPOSITO.monto_actual > 0){
-        this.pagosSelect.forEach((i)=>{
-          saa =  this.DEPOSITO.monto_actual;
-          sar = 0;
-          if (i.saldo_actual > 0 && this.DEPOSITO.monto_actual > 0){
-            //verifica si el pago esta en moRA
-            if (i.mora > 0 && i.estado === 2){
-              mora = i.mora;
-              if (this.DEPOSITO.monto_actual >= i.mora) {
-                sar = (parseFloat(i.mora) + parseFloat(sar)).toFixed(2)
-                i.mora = 0;
-                this.DEPOSITO.monto_actual = (parseFloat(this.DEPOSITO.monto_actual) - parseFloat(mora)).toFixed(2);
-              }else if (this.DEPOSITO.monto_actual < i.mora) {
-                sar = (parseFloat(this.DEPOSITO.monto_actual) + parseFloat(sar)).toFixed(2)
-                i.mora = (parseFloat(i.mora) - parseFloat(this.DEPOSITO.monto_actual)).toFixed(2);
-                this.DEPOSITO.monto_actual = (parseFloat(this.DEPOSITO.monto_actual) - (parseFloat(mora) - parseFloat(i.mora))).toFixed(2)
-              }
-              if (i.mora === 0)
-                i.is_mora = false
+    abrirNavegador(clave, recibo){
+      let url = '';
+      url = this.$axios.defaults.baseURL + 'documentos/cajas/recibos/usuario=' + this.USUARIO + '/recibo=' + recibo + '/' + clave;
 
-              i.saldo_actual = (parseFloat(i.saldo_actual) - parseFloat(mora)).toFixed(2);
-              this.moraAbonado = (parseFloat(mora) - parseFloat(i.mora)).toFixed(2);
-            }
-
-            if (this.DEPOSITO.monto_actual > 0){
-              sa = i.saldo_actual;
-              if (this.DEPOSITO.monto_actual >= i.saldo_actual) {
-                sar = (parseFloat(i.saldo_actual) + parseFloat(sar)).toFixed(2)
-                i.saldo_actual = 0;
-                this.DEPOSITO.monto_actual = (parseFloat(this.DEPOSITO.monto_actual) - parseFloat(sa)).toFixed(2);
-              }if (this.DEPOSITO.monto_actual < i.saldo_actual) {
-                sar = (parseFloat(this.DEPOSITO.monto_actual) + parseFloat(sar)).toFixed(2)
-                i.saldo_actual = (parseFloat(i.saldo_actual) - parseFloat(this.DEPOSITO.monto_actual)).toFixed(2);
-                this.DEPOSITO.monto_actual = (parseFloat(this.DEPOSITO.monto_actual) - (parseFloat(sa) - parseFloat(i.saldo_actual))).toFixed(2);
-              }
-
-              if (this.DEPOSITO.monto_actual < 0)
-                this.DEPOSITO.monto_actual = 0;
-            }
-
-            if (i.saldo_actual === 0)
-              i.estado = 3;
-
-            this.saldo_abonado = (parseFloat(this.saldo_abonado) + (parseFloat(saa) - parseFloat(this.DEPOSITO.monto_actual))).toFixed(2)
-
-            this.pagosSi.push({
-              pago_id: i.id,
-              saldo_actual: i.saldo_actual,
-              is_mora: i.is_mora,
-              mora:    i.mora,
-              estado:  i.estado,
-              abonado: sar,
-              detalle: detalle,
-            })
-          }
-        })
-      }
-
-      if (this.DEPOSITO.monto_actual < 0)
-        this.DEPOSITO.monto_actual = 0;
+      ipcRenderer.send('pint_navegador', url);
+      this.$store.commit('activarOverlay', false);
     },
     cargarCuentas(){
       this.$axios.get('cuentas').then((res)=>{
@@ -241,21 +237,110 @@ export default {
         this.loadCuentas = false;
       })
     },
+    distribuirPago(){
+      if (this.DEPOSITO.monto_actual > 0){
+        let saldo = this.DEPOSITO.monto_actual, estado = true;
+        let pagos = this.pagosSelect;
+        let pago = 0, saldo_p = 0;
+        this.PagosDistrubuidos = [];
+        pagos.forEach((item)=>{
+          console.log(item)
+          if (saldo > 0){
+            if (item.is_mora === 1 && item.mora > 0) {
+              if (saldo > item.mora) {
+                pago = item.mora;
+                saldo_p = 0
+              }else {
+                pago = saldo;
+                saldo_p = (pago - item.mora).toFixed(2)
+                if (saldo_p < 0)
+                  saldo_p = (item.mora - pago).toFixed(2)
+              }
+
+              saldo = (saldo - pago).toFixed(2);
+              if (saldo < 0)
+                saldo = 0;
+
+              this.PagosDistrubuidos.push({
+                status: 2,
+                pago_id: item.id,
+                tipo: 'Mora',
+                detalle: 'Mora ' + item.detalle,
+                pendiente: item.mora,
+                pagara:    pago,
+                saldo:     saldo_p,
+                hay:       saldo
+              })
+            }
+
+            if (saldo > 0 && item.estado === 2 || item.estado === 1){
+              if (saldo > item.saldo_cap){
+                pago = item.saldo_cap;
+                saldo_p = 0;
+              }else{
+                pago = (item.saldo_cap - saldo).toFixed(2);
+                saldo_p = (pago - item.saldo_cap).toFixed(2);
+                if (saldo_p < 0){
+                  pago = saldo
+                  saldo_p = (item.saldo_cap - pago).toFixed(2)
+                }
+              }
+
+              saldo = (saldo - pago).toFixed(2);
+
+              this.PagosDistrubuidos.push({
+                status: 1,
+                pago_id: item.id,
+                tipo: 'Letra',
+                detalle: item.detalle,
+                pendiente: item.saldo_cap,
+                pagara:    pago,
+                saldo:     saldo_p,
+                hay:       saldo
+              })
+            }
+          }
+        });
+      }
+    },
     registrarForm(){
       this.$store.commit('activarOverlay', true);
       this.$axios.put('venta/pagos/'+this.DEPOSITO.venta.id,{
-        pagos: this.pagosSi,
-        saldo_abonado: this.saldo_abonado,
-        saldo_actual:  this.DEPOSITO.monto_actual,
+        pagos: this.PagosDistrubuidos,
         observacion:   this.DEPOSITO.observacion,
-        mora_abonado:  this.mora_abonado,
-        pagos_json: JSON.stringify(this.pagosSi),
+        pagos_json: JSON.stringify(this.PagosDistrubuidos),
+        cuenta_banco:  this.cuenta,
+        deposito:      this.DEPOSITO.id,
 
-        total:         this.saldo_abonado,
+        total:         this.DEPOSITO.monto,
         sucursal_id:   this.CUENTA.sucursal_id,
         caja_id:       this.DEPOSITO.caja_id,
       }).then((res)=>{
-        this.$store.commit('activarOverlay', false)
+        this.$store.commit('contabilidad/depositos/cargar_DEPOSITOS')
+        this.abrirNavegador(res.data.clave, res.data.codigo);
+        this.enviado = true;
+      }).catch((error)=>{
+        this.$store.commit('activarOverlay', false);
+        this.$store.commit('notificacion',{texto:'Hubo un error en el servidor', color:'error'})
+      })
+    },
+    validarForm(){
+      if (this.$refs.FormValidarDepostio.validate())
+        this.registrarForm();
+      else
+        this.$store.commit('notificacion',{texto:'Hace falta datos', color:'warning'});
+    },
+    verDocumento(URL){
+      this.$store.commit('activarOverlay', true);
+      this.$axios.post('leer_documento/',
+          {ubicacion: URL}).then((res)=>{
+        if (res.status === 200){
+          ipcRenderer.send('pint_navegador', res.data.url);
+          this.$store.commit('activarOverlay', false);
+        }
+      }).catch((error)=>{
+        this.notificacion('Ocurrió un error al cargar el documento','error');
+        this.$store.commit('activarOverlay', false);
       })
     }
   }
